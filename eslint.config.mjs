2bb2a9fd348d15c5996a1eee312c64e68f0d65c5
Abhishek Kumar Pandit <akp310899@gmail.com>
2026-01-21 00:12:17 +0530

    code fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript.js";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   nextVitals,
   nextTs,
   globalIgnores([
@@ -12,5 +12,3 @@ const eslintConfig = defineConfig([
     "next-env.d.ts",
   ]),
 ]);
-
-export default eslintConfig;
